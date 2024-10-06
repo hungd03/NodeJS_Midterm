71db1424950dc30b22d58462d3fb21ccd5053e50
hungd03 <hungcongdinh12@gmail.com>
2024-10-06 16:25:25 +0700

    Add courseRouter

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
-app.use("api/courses", courseRouter);
+app.use("/api/courses", courseRouter);
 
 const connectDB = async () => {
     try {
