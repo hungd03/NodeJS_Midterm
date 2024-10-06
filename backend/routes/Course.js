4d6773264e032c6655097c4d8befbcd9c0c4a376
hungd03 <hungcongdinh12@gmail.com>
2024-10-06 18:14:35 +0700

    Create course router

---
+++ backend/routes/Course.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+const Course = require('../models/Course');
+
+// @route POST api/courses
+// @desc Create course
+// @access private
+router.post('/', async (req, res) => {
+   
+    const { tilte, description, url, status } = req.body;
+
+    if (!tilte)
+        return res.status(400).json({ success: false, message: "Title is required" });
+
+    try {
+
+        const newCourse = Course({
+            tilte,
+            description,
+            url: url.startsWith("https://") ? url : `https://${url} `,
+            status: status || "TO LEARN",
+            user: "67024aae63261db40e08e8c4",
+        });
+        await newPost.save();
+
+        res.json({ success: true, message: "Add course successfully", course: newCourse });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
+})
+
+module.exports = router;
