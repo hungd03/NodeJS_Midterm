1c1f1136df3f1de821fe1dcff5547dbc5012c1f7
hungd03 <hungcongdinh12@gmail.com>
2024-10-07 19:16:22 +0700

    update /routes/Auth.js

--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -42,32 +42,36 @@ router.post('/register', async (req, res) => {
 
 // @route POST api/auth/login
 // @desc Login user
-router.post('/login', async (req, res) => {
-    const { username ,password } = req.body;
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
 
-    if (!username || !password) 
-        return res.status(400).json({ success: false, message: "Missing username or password" });
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Missing username or password" });
+  }
 
-    try {
-        // Check us and pwd
-        const user = await User.findOne({username});
-        if (!user)  
-            return res.status(400).json({ success: false, message: "Incorrect username or password" });
-
-        // Found user
-        const passwordValid = await argon2.verify(user.password, password);
-        if (!passwordValid)
-            return res.status(400).json({ success: false, message: "Incorrect username or password" });
-
-        // Return token
-        const accessToken = jwt.sign({userId: user._id}, process.env.ACCESS_TOKEN_SECRET);
-
-        res.json({ success: true, message: "Logged in successfully", accessToken });
-
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ success: false, message: "Internal server error" });
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Incorrect username or password" });
     }
+
+    const passwordValid = await argon2.verify(user.password, password);
+    if (!passwordValid) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Incorrect username or password" });
+    }
+
+    const accessToken = jwt.sign( { userId: user._id }, process.env.ACCESS_TOKEN_SECRET);
+    res.json({ success: true, message: "Logged in successfully", accessToken });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
 });
 
 module.exports = router;
