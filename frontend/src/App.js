43ef3dee23f4203023f6eb9e8588f10c407fadc1
hungd03 <hungcongdinh12@gmail.com>
2024-10-07 17:22:33 +0700

    Update code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Landing from "./components/layout/Landing";
+import Auth from "./views/Auth";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+        <Route exact path="/login" element={<Auth authRoute="login" />} />
+        <Route exact path="/register" element={<Auth authRoute="register" />} />
+      </Routes>
+    </Router>
   );
 }
 
