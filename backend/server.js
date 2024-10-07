fa4fb81112aefa4b4013c0b9be7b774e801111ac
hungd03 <hungcongdinh12@gmail.com>
2024-10-07 19:15:27 +0700

    add cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
 require("dotenv").config();
 
 const authRouter = require('./routes/Auth');
@@ -9,6 +10,7 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Routers
 app.use("/api/auth", authRouter);
