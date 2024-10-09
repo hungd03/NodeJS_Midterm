8cc6a75e8b85a6fba32e4abfaa0b75821bbf93da
hungd03 <hungcongdinh12@gmail.com>
2024-10-09 22:51:12 +0700

    update code AuthContext.jsx

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,7 +1,8 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 import { authReducer } from "../reducers/authReducer";
 import { apiUrl, LOCAL_STORAGE_TOKEN_NAME } from "./constants";
 import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
 
 export const AuthContext = createContext();
 
@@ -12,15 +13,55 @@ const AuthContextProvider = ({ children }) => {
     user: null,
   });
 
+  // Authenticate user
+  useEffect(() => {
+    const loadUser = async () => {
+      if (localStorage[LOCAL_STORAGE_TOKEN_NAME]) {
+        setAuthToken(localStorage[LOCAL_STORAGE_TOKEN_NAME]);
+      } else {
+        dispatch({
+          type: "SET_AUTH",
+          payload: { isAuthenticated: false, user: null },
+        });
+        return;
+      }
+
+      try {
+        const res = await axios.get(`${apiUrl}/auth`);
+        if (res.data.success) {
+          dispatch({
+            type: "SET_AUTH",
+            payload: { isAuthenticated: true, user: res.data.user },
+          });
+        }
+      } catch (error) {
+        localStorage.removeItem(LOCAL_STORAGE_TOKEN_NAME);
+        setAuthToken(null);
+        dispatch({
+          type: "SET_AUTH",
+          payload: { isAuthenticated: false, user: null },
+        });
+      }
+    };
+
+    loadUser();
+  }, []);
+
   // Login
   const loginUser = async (userForm) => {
     try {
       const res = await axios.post(`${apiUrl}/auth/login`, userForm);
 
-      if (res.data.success) 
+      if (res.data.success) {
         localStorage.setItem(LOCAL_STORAGE_TOKEN_NAME, res.data.accessToken);
-      
-      return res.data;
+        setAuthToken(res.data.accessToken);
+        dispatch({
+          type: "SET_AUTH",
+          payload: { isAuthenticated: true, user: res.data.user },
+        });
+
+        return res.data;
+      }
     } catch (error) {
       if (error.response.data) {
         return error.response.data;
@@ -29,9 +70,9 @@ const AuthContextProvider = ({ children }) => {
       }
     }
   };
-  
+
   // Context data
-  const authContextData = {loginUser};
+  const authContextData = { loginUser, authState };
 
   // Return provider
   return (
