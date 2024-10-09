3349b5f033bf0446a1d3ef1a06e1b44282fe262a
hungd03 <hungcongdinh12@gmail.com>
2024-10-09 22:52:44 +0700

    update auth

--- frontend/src/views/Auth.jsx
+++ frontend/src/views/Auth.jsx
@@ -1,15 +1,30 @@
 import LoginForm from "../components/auth/LoginForm";
 import RegisterForm from "../components/auth/RegisterForm";
+import { AuthContext } from "../contexts/AuthContext";
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import Spinner from "react-bootstrap/Spinner";
 
 const Auth = ({ authRoute }) => {
+  const {
+    authState: { authLoading, isAuthenticated },
+  } = useContext(AuthContext);
   let body;
 
-  body = (
-    <>
-      {authRoute === "login" && <LoginForm />}
-      {authRoute === "register" && <RegisterForm />}
-    </>
-  );
+  if (authLoading)
+    body = (
+      <div className="d-flex justify-conten-center mt-2">
+        <Spinner animation="border" variant="info" />
+      </div>
+    );
+  else if (isAuthenticated) return <Navigate to="/dashboard" />;
+  else
+    body = (
+      <>
+        {authRoute === "login" && <LoginForm />}
+        {authRoute === "register" && <RegisterForm />}
+      </>
+    );
 
   return (
     <div className="landing">
