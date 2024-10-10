8b0b6a7501b6d64066b42ade8b5be2d10ec55563
hungd03 <hungcongdinh12@gmail.com>
2024-10-10 16:08:10 +0700

    update code

--- frontend/src/contexts/constants.jsx
+++ frontend/src/contexts/constants.jsx
@@ -1,3 +1,4 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:8080/api' : '';
+export const apiUrl =
+  process.env.NODE_ENV !== "production" ? "http://localhost:8080/api" : "";
 
-export const LOCAL_STORAGE_TOKEN_NAME = 'token';
+export const LOCAL_STORAGE_TOKEN_NAME = "token";
